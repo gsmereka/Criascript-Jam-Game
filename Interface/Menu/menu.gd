191080cedaf637eda8fcc0933f3a768ba0b5f4ae
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2024-10-05 15:30:46 -0300

    IntroMaked

--- Interface/Menu/menu.gd
+++ Interface/Menu/menu.gd
@@ -1,49 +1,35 @@
 extends Control
 
-# Lista de caminhos para os nodes que você quer instanciar
-var node_paths: Array = [
-	"res://Interface/LenguageMenu/lenguage_menu.tscn",
-	"res://Interface/ResolutionMenu/resoltutionMenu.tscn",
-	"res://Interface/Menu/menu_options.tscn"
-]
-
-var node_names: Array = [
-	"LenguageMenu",
-	"ResolutionMenu",
-	"MenuOptions"
-]
+const LenguageMenu := "res://Interface/LenguageMenu/lenguage_menu.tscn"
+const ResolutionMenu := "res://Interface/ResolutionMenu/resoltutionMenu.tscn"
+const MenuMenu := "res://Interface/Menu/menu_options.tscn"
 
 var current_index: int = 0
 var current_node: Node = null
 
-func _ready():
-	# Inicia o processo chamando o primeiro node
-	#_instanciar_proximo_node()
+func _load_intro():
+	var MenuNode: Menu
+	MenuNode = preload(MenuMenu).instantiate()
+	add_child(MenuNode)
+	var ResolutionNode: Menu
+	ResolutionNode = preload(ResolutionMenu).instantiate()
+	ResolutionNode.backup_button = MenuNode.init_button
+	add_child(ResolutionNode)
+	var LenguageNode: Menu
+	LenguageNode = preload(LenguageMenu).instantiate()
+	LenguageNode.backup_button = ResolutionNode.init_button
+	add_child(LenguageNode)
 	pass
-	
 
-func _process(delta):
-	if current_index == 0:
+func _ready():
+	if Global.first_play:
+		_load_intro()
+		Global.first_play = false
+	else:
 		var node = preload("res://Interface/Menu/menu_options.tscn").instantiate()
 		add_child(node)
-		current_index += 1
+	pass
+
+func _process(delta):
 	# Verifica se o node atual foi concluído
 	return
-	if (current_index - 1 < node_paths.size()) && !self.get_node_or_null(node_names[current_index - 1]):
-		_on_node_finalizado()
-
-func _instanciar_proximo_node():
-	# Verifica se ainda há nodes na lista
-	if current_index < node_paths.size():
-		var node_scene = load(node_paths[current_index])
-		current_node = node_scene.instantiate()
-		
-		# Adiciona o node à cena
-		add_child(current_node)
-		
-		# Incrementa o índice para o próximo node
-		current_index += 1
-
-func _on_node_finalizado():
-	# Chama a função para instanciar o próximo node
-	_instanciar_proximo_node()
