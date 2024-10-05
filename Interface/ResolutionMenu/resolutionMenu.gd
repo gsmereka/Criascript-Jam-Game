8ac40c3a57c682a17a6298557923452a1f517ee3
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2024-10-05 15:08:09 -0300

    finished_resolution_window

--- Interface/ResolutionMenu/resolutionMenu.gd
+++ Interface/ResolutionMenu/resolutionMenu.gd
@@ -8,3 +8,25 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_full_screen_pressed() -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	pass # Replace with function body.
+
+
+func _on_p_pressed() -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	DisplayServer.window_set_size(Vector2(1920, 1080))
+	pass # Replace with function body.
+
+
+func _on_720p_pressed() -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	DisplayServer.window_set_size(Vector2(1280, 720))
+	pass # Replace with function body.
+
+
+func _on_close_pressed() -> void:
+	_exit_menu()
+	pass # Replace with function body.
