435d71e2abbb2b838c1732ca18377e5af556f5b1
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2024-10-05 14:53:13 -0300

    update-resolutuon-menu

--- Interface/ResolutionMenu/resolutionMenu.gd
+++ Interface/ResolutionMenu/resolutionMenu.gd
@@ -5,19 +5,6 @@ func _ready() -> void:
 	#init_button.grab_focus()
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _on_full_screen_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_language_pressed() -> void:
-	pass # Replace with function body.
-
-func _on_exit_config_pressed() -> void:
-	#get_tree().paused = false
-	_exit_menu()
-	pass # Replace with function body.
