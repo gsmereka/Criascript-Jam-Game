9968734c7d31a7b3b089158789a18374c69cf632
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2024-10-04 23:59:21 -0300

    responsive-buttons

--- Interface/Menu/menu.gd
+++ Interface/Menu/menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var init_button := $ConfigPanel/OptionsContainer/FullScreen
+@onready var init_button := $ConfigPanel/OptionsContainer/Init
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,16 +13,17 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_full_screen_pressed() -> void:
+func _on_init_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_language_pressed() -> void:
+func _on_config_pressed() -> void:
+	#get_tree().paused = true
+	var node = preload("res://Interface/ConfigMenu/config_menu.tscn").instantiate()
+	get_parent().add_child(node)
 	pass # Replace with function body.
 
-func _on_exit_config_pressed() -> void:
-	#get_tree().paused = false
-	get_parent().get_node("MenuOptions").init_button.grab_focus()
-	get_parent().remove_child(self)
-	queue_free()
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
 	pass # Replace with function body.
