e84cfbcfbe58248ec724e57e30ff437d5192fa40
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2024-10-04 22:28:34 -0300

    update-config-menu

---
+++ Interface/ConfigMenu/config_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@onready var init_button := $ConfigPanel/OptionsContainer/FullScreen
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	init_button.grab_focus()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
