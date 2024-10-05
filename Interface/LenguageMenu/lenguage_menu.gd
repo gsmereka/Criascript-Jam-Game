5a663573f424723d3bfcd9bf2bc293859ae12a4a
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2024-10-05 17:00:13 -0300

    add-music

--- Interface/LenguageMenu/lenguage_menu.gd
+++ Interface/LenguageMenu/lenguage_menu.gd
@@ -7,10 +7,17 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_english_pressed() -> void:
+	change_language("en")
 	_exit_menu()
 	pass # Replace with function body.
 
 
 func _on_portuguese_pressed() -> void:
+	change_language("pt_BR")
 	_exit_menu()
 	pass # Replace with function body.
+
+func change_language(lang: String) -> void:
+	TranslationServer.set_locale(lang)
+	pass
+	
